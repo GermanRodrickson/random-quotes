edd5d25e4fa5dadabd2ec061f6dc056745d795e5
Germán <germanrodrickson@gmail.com>
2019-05-03 13:49:53 +0200

    rename

---
+++ docs/precache-manifest.d5b16525901179ec134ba82a594b300c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ae1c550106f2e27236e8d9bc2e026aa",
+    "url": "/random-quotes/index.html"
+  },
+  {
+    "revision": "19b47055e3586c908fb8",
+    "url": "/random-quotes/static/css/main.6f37084f.chunk.css"
+  },
+  {
+    "revision": "eb72fd191f6a9f346ecb",
+    "url": "/random-quotes/static/js/2.57e09ab8.chunk.js"
+  },
+  {
+    "revision": "19b47055e3586c908fb8",
+    "url": "/random-quotes/static/js/main.af45b7d1.chunk.js"
+  },
+  {
+    "revision": "9fcc4046b366da42450f",
+    "url": "/random-quotes/static/js/runtime~main.042febda.js"
+  }
+]);
