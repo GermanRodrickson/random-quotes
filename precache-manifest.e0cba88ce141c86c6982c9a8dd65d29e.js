db05ba813a47c336fefc3bc6645a6b1da9108168
Germán <germanrodrickson@gmail.com>
2019-05-03 12:11:45 +0200

    Updates

---
+++ precache-manifest.e0cba88ce141c86c6982c9a8dd65d29e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21f791d3f81c477eb7320a43c5f5cba6",
+    "url": "/random-quotes/index.html"
+  },
+  {
+    "revision": "ed2e2878d805cb3f4388",
+    "url": "/random-quotes/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/random-quotes/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "ed2e2878d805cb3f4388",
+    "url": "/random-quotes/static/js/main.5c4878fd.chunk.js"
+  },
+  {
+    "revision": "9fcc4046b366da42450f",
+    "url": "/random-quotes/static/js/runtime~main.042febda.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/random-quotes/static/media/logo.5d5d9eef.svg"
+  }
+]);
